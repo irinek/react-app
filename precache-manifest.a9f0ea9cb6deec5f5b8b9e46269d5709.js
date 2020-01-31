63b781e0b9d7a41eca12b7c4ebe21ff7925b976e
irinek <irinek@o2.pl>
2020-01-31 19:56:33 +0100

    Updates

---
+++ precache-manifest.a9f0ea9cb6deec5f5b8b9e46269d5709.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808afda18e7eeca85e4ca04f1b5efa29",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "b4e978a305c1dc298060",
+    "url": "/react-app/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "78fd5702e3a0de1e2f90",
+    "url": "/react-app/static/js/2.e2b2308f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-app/static/js/2.e2b2308f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e978a305c1dc298060",
+    "url": "/react-app/static/js/main.0d46fd72.chunk.js"
+  },
+  {
+    "revision": "147aa707a3c93a471937",
+    "url": "/react-app/static/js/runtime-main.61f57482.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
